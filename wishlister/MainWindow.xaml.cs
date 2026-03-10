5e840c2c307ed34f10f80dda9bc13fc741d319e6
Htonj <cosmopolitesss34@gmail.com>
2026-03-10 16:58:21 +0000

    0.0.1

---
+++ wishlister/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wishlister
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static public List<Wish> wishes = new List<Wish>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //debug
+            wishes.Add(new Wish("jhadfkjhs", "5478$"));
+            wishes.Add(new Wish("jauka nsdkja", "1$"));
+            wishes.Add(new Wish("'alisdan dkdls'", "467$"));
+            foreach (Wish wish in wishes) {
+                WishStack.Children.Add(wish);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Adder adder = new Adder();
+            adder.Show();
+        }
+    }
+}
---
+++ wishlister/Wish.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wishlister
+{
+    /// <summary>
+    /// Логика взаимодействия для Wish.xaml
+    /// </summary>
+    public partial class Wish : UserControl
+    {
+        public Wish(string names, string prices)
+        {
+            InitializeComponent();
+            price.Content = prices;
+            namexD.Content = names;
+
+        }
+    }
+}
